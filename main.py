6354d6e1ddac9d24140d9b62d325c19420a70e00
MarcusPBF <Marcus.Miles@pbfenergy.com>
2024-06-07 14:22:36 -0400

    added admin user protected functionality

--- main.py
+++ main.py
@@ -118,6 +118,7 @@ def get_all_posts():
 
 # TODO: Allow logged-in users to comment on posts
 @app.route("/post/<int:post_id>")
+@login_required
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
     return render_template("post.html", post=requested_post)
